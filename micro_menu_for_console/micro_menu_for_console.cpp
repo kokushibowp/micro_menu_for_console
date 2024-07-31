1b849e8108e8d0c976296476808365e6f7ad8233
Kirill Artamonov <kokushibowp@gmail.com>
2024-07-31 15:30:07 +0300

    Добавьте файлы проекта.

---
+++ micro_menu_for_console/micro_menu_for_console.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Menu.h"
+
+int main()
+{
+    system("chcp 1251 > 0");
+    COORD menuPos = { 2, 1 };
+    Menu menu(menuPos);
+    menu.add_item((char*)"Один");
+    menu.add_item((char*)"Два");
+    menu.add_item((char*)"Три Четыре Пять Шесть");
+    menu.add_item((char*)"Семь Восемь");
+    menu.set_color(0x0F, 0x0E);
+    menu.draw_menu();
+    menu.set_cursor(1);
+    while (1) {
+        if ((GetKeyState(VK_UP) == -127) || (GetKeyState(VK_UP) == -128)) {
+            menu.move_up();
+            Sleep(150);
+        }
+        else if ((GetKeyState(VK_DOWN) == -127) || (GetKeyState(VK_DOWN) == -128)) {
+            menu.move_down();
+            Sleep(150);
+        }
+        else if ((GetKeyState(VK_RETURN) == -127) || (GetKeyState(VK_RETURN) == -128)) {
+            menu.clear_screen();
+            std::cout << "какой-то код\nномер выбранного пункта: " << menu.get_cursor_pos() << "\n";
+            system("PAUSE");
+            system("CLS");
+            menu.draw_menu();
+            menu.set_cursor(menu.get_cursor_pos());
+            Sleep(150);
+        }
+    }
+}
